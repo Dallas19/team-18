40fe73a1fc3c4c07569a85d64eba9ad4a8657544
teatep <37784305+teatep@users.noreply.github.com>
2019-10-19 01:49:29 -0500

    Implemented Elisha's CSS

--- src/App.js
+++ src/App.js
@@ -8,43 +8,62 @@ import {
 
 class App extends React.Component{
   render(){
-    // return <Button title="hello"/>
     return(
-    <div>
-    <style type="text/css">
-      {`
-        .btn-flat {
-          background-color: purple;
-          color: white;
-        }
 
-        .btn-xxl {
-          padding: 1rem 1.5rem;
-          font-size: 1.5rem;
-        }
-      `}
-    </style>
+      <html>
+        <style type="text/css">
+        {`
+              #emergency {
+                font-family: 'Dancing Script', cursive;
+                font-size: 40px;
+                color: Black;
+                background: none;
+                border-radius: 10px;
+                padding: 2.0em;
+                outline: none;
+                background-color: yellow;
+              }
 
-    <Button variant="flat" size="xxl">
-      start
-    </Button>
-    <style type="text/css">
-    {`
-      .btn-flat {
-      background-color: blue;
-      color: white;
-      }
+              .emergency {
+                display: inline-block;
+                height: 100%;
+                align-items: center;
+                font-size: 2em;
+                margin: 20;
+                float: left;
+              }
 
-      .btn-xxl {
-      padding: 1rem 1.5rem;
-      font-size: 1.5rem;
-      }
-    `}
-    </style>
-    <Button variant = "flat" size="xxl"> Start </Button>
+              #Dog {
+                font-family: 'Dancing Script', cursive;
+                font-size: 40px;
+                color: Black;
+                background: none;
+                border-radius: 10px;
+                padding: 2.0em;
+                outline: none;
+                background-color: Blue;
+              }
 
-  </div>
-);
+              .Dog container{
+                display-inline: block;
+                height: 100%;
+                align-items: center;
+                font-size: 2em;
+                float: right;
+              }
+
+        `}
+        </style>
+        <div class="button">
+          <button id="emergency">Emergency</button>
+        </div>
+
+        <div class="button">
+          <button id="Dog">Dog</button>
+        </div>
+      </html>
+
+      );
   }
 }
 

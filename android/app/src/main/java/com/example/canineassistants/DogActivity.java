de52850c1dbfa0b58d290c9926116942a6cdfb04
Gizdich <john.gizdich@utexas.edu>
2019-10-19 05:16:26 -0500

    Init Android

---
+++ android/app/src/main/java/com/example/canineassistants/DogActivity.java
@@ -0,0 +1,24 @@
+package com.example.canineassistants;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class DogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dog);
+    }
+
+    public void leftPath(View view){
+
+    }
+
+    public void rightPath(View view){
+
+    }
+}
